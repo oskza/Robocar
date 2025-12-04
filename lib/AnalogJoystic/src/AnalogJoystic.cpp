8b5f7b25551c6b7b4099fe88fceb29cf68563f17
oskza <dev@oskza.com>
2025-12-04 20:57:08 +0200

    refactored AnalogJoystic class

--- lib/AnalogJoystic/src/AnalogJoystic.cpp
+++ lib/AnalogJoystic/src/AnalogJoystic.cpp
@@ -4,47 +4,16 @@ AnalogJoystic::AnalogJoystic(uint8_t vertPin, uint8_t horzPin, uint16_t deadzone
         : _vertPin(vertPin), _horzPin(horzPin), _deadzone(deadzone), 
             _vertCenter(2048), _horzCenter(2048) {}
 
+void AnalogJoystic::calibrateVerticalCenter() { _vertCenter = analogRead(_vertPin); }
+void AnalogJoystic::calibrateHorizontalCenter() { _horzCenter = analogRead(_horzPin); }
 void AnalogJoystic::calibrateCenter() {
     calibrateVerticalCenter();
     calibrateHorizontalCenter();
 }
 
-void AnalogJoystic::calibrateVerticalCenter() { _vertCenter = analogRead(_vertPin); }
-
-void AnalogJoystic::calibrateHorizontalCenter() { _horzCenter = analogRead(_horzPin); }
-
-uint16_t AnalogJoystic::readVertical() const { return analogRead(_vertPin); }
-
-uint16_t AnalogJoystic::getVerticalCenter() const { return _vertCenter; }
-
-uint16_t AnalogJoystic::getVerticalDeadzoneMin() const { return _vertCenter - _deadzone; }
-
-uint16_t AnalogJoystic::getVerticalDeadzoneMax() const { return _vertCenter + _deadzone; }
-
-uint16_t AnalogJoystic::readHorizontal() const { return analogRead(_horzPin); }
-
-uint16_t AnalogJoystic::getHorizontalCenter() const { return _horzCenter; }
-
-uint16_t AnalogJoystic::getHorizontalDeadzoneMin() const { return _horzCenter - _deadzone; }
-
-uint16_t AnalogJoystic::getHorizontalDeadzoneMax() const { return _horzCenter + _deadzone; }
+uint16_t AnalogJoystic::readVertical() const { return analogRead(_vertPin) - _vertCenter; }
+uint16_t AnalogJoystic::readHorizontal() const { return analogRead(_horzPin) - _horzCenter; }
 
 bool AnalogJoystic::isInDeadzone(uint16_t vert, uint16_t horz) const {
-    return isVerticalInDeadzone(vert) && isHorizontalInDeadzone(horz);
+    return abs(vert) < _deadzone && abs(horz) < _deadzone;
 }
-
-bool AnalogJoystic::isVerticalInDeadzone(uint16_t val) const { 
-    return !isVerticalBelowDeadzone(val) && !isVerticalAboveDeadzone(val);
-}
-
-bool AnalogJoystic::isVerticalBelowDeadzone(uint16_t val) const { return val < getVerticalDeadzoneMin(); }
-
-bool AnalogJoystic::isVerticalAboveDeadzone(uint16_t val) const { return val > getVerticalDeadzoneMax(); }
-
-bool AnalogJoystic::isHorizontalInDeadzone(uint16_t val) const {
-    return !isHorizontalBelowDeadzone(val) && !isHorizontalAboveDeadzone(val);
-}
-
-bool AnalogJoystic::isHorizontalBelowDeadzone(uint16_t val) const { return val < getHorizontalDeadzoneMin(); }
-
-bool AnalogJoystic::isHorizontalAboveDeadzone(uint16_t val) const { return val > getHorizontalDeadzoneMax(); }
--- lib/AnalogJoystic/src/AnalogJoystic.h
+++ lib/AnalogJoystic/src/AnalogJoystic.h
@@ -9,28 +9,13 @@ private:
 public:
   AnalogJoystic(uint8_t vertPin, uint8_t horzPin, uint16_t deadzone = 80);
 
-  void calibrateCenter();
   void calibrateVerticalCenter();
   void calibrateHorizontalCenter();
+  void calibrateCenter();
 
   uint16_t readVertical() const;
-  uint16_t getVerticalCenter() const;
-  uint16_t getVerticalDeadzoneMin() const;
-  uint16_t getVerticalDeadzoneMax() const;
-
   uint16_t readHorizontal() const;
-  uint16_t getHorizontalCenter() const;
-  uint16_t getHorizontalDeadzoneMin() const;
-  uint16_t getHorizontalDeadzoneMax() const;
 
   bool isInDeadzone(uint16_t vert, uint16_t horz) const;
-
-  bool isVerticalInDeadzone(uint16_t val) const;
-  bool isVerticalBelowDeadzone(uint16_t val) const;
-  bool isVerticalAboveDeadzone(uint16_t val) const;
-
-  bool isHorizontalInDeadzone(uint16_t val) const;
-  bool isHorizontalBelowDeadzone(uint16_t val) const;
-  bool isHorizontalAboveDeadzone(uint16_t val) const;
 };
 #endif
