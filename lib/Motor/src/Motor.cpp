d56646f94d1e9edcf1fac2932b31b6a15201b8fc
oskza <dev@oskza.com>
2025-12-04 20:25:37 +0200

    added pwm correction to Motor class

--- lib/Motor/src/Motor.cpp
+++ lib/Motor/src/Motor.cpp
@@ -1,8 +1,17 @@
 #include "Motor.h"
 
-Motor::Motor(uint8_t pwmPin, uint8_t fwdPin, uint8_t bckPin, uint8_t pwmChannel) 
-        : _pwmPin(pwmPin), _fwdPin(fwdPin), _bckPin(bckPin), _pwmChannel(pwmChannel) {}
-        
+Motor::Motor(uint8_t pwmPin, uint8_t fwdPin, uint8_t bckPin, uint8_t pwmChannel, float corr) 
+        : _pwmPin(pwmPin), _fwdPin(fwdPin), _bckPin(bckPin), _pwmChannel(pwmChannel), _corr(corr) {}
+
+uint8_t Motor::_correctPWM(uint8_t pwm) {
+    int res = (int)round(pwm * _corr);
+    return (res > MOTOR_MAX_PWM)
+            ? MOTOR_MAX_PWM
+            : (res < MOTOR_MIN_PWM)
+                ? MOTOR_MIN_PWM
+                : (uint8_t)res;
+}
+
 void Motor::init(uint32_t freq, uint8_t res) {
     pinMode(_fwdPin, OUTPUT);
     pinMode(_bckPin, OUTPUT);
@@ -11,20 +20,27 @@ void Motor::init(uint32_t freq, uint8_t res) {
     stop();
 }
 
+void Motor::stop() {
+    ledcWrite(_pwmChannel, 0);
+    digitalWrite(_fwdPin, LOW);
+    digitalWrite(_bckPin, LOW);
+}
+
 void Motor::forward(uint8_t pwm) {
-    ledcWrite(_pwmChannel, pwm);
+    ledcWrite(_pwmChannel, _correctPWM(pwm));
     digitalWrite(_fwdPin, HIGH);
     digitalWrite(_bckPin, LOW);
 }
 
 void Motor::backward(uint8_t pwm) {
-    ledcWrite(_pwmChannel, pwm);
+    ledcWrite(_pwmChannel, _correctPWM(pwm));
     digitalWrite(_fwdPin, LOW);
     digitalWrite(_bckPin, HIGH);
 }
 
-void Motor::stop() {
-    ledcWrite(_pwmChannel, 0);
-    digitalWrite(_fwdPin, LOW);
-    digitalWrite(_bckPin, LOW);
+void Motor::rotate(int16_t pwm) {
+    if(pwm >= 0) 
+        forward(_correctPWM(pwm)); 
+    else 
+        backward(_correctPWM(-pwm));
 }
--- lib/Motor/src/Motor.h
+++ lib/Motor/src/Motor.h
@@ -2,14 +2,20 @@
 #define MOTOR_H
 #include <Arduino.h>
 
+#define MOTOR_MIN_PWM 110
+#define MOTOR_MAX_PWM 255
+
 class Motor {
 private:
   uint8_t _pwmPin, _fwdPin, _bckPin, _pwmChannel;
+  float _corr;
+  uint8_t _correctPWM(uint8_t pwm);
 public:
-  Motor(uint8_t pwmPin, uint8_t fwdPin, uint8_t bckPin, uint8_t pwmChannel);
+  Motor(uint8_t pwmPin, uint8_t fwdPin, uint8_t bckPin, uint8_t pwmChannel, float corr = 1);
   void init(uint32_t freq, uint8_t res);
+  void stop();
   void forward(uint8_t pwm);
   void backward(uint8_t pwm);
-  void stop();
+  void rotate(int16_t pwm);
 };
 #endif
