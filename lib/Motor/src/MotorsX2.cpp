c63b3bdde22d17821b6032247c4aba7376d38cd3
oskza <dev@oskza.com>
2025-12-05 22:16:46 +0200

    Removed moveForMs from MotorsX2

--- lib/Motor/src/MotorsX2.cpp
+++ lib/Motor/src/MotorsX2.cpp
@@ -1,7 +1,7 @@
 #include "MotorsX2.h"
 
-MotorsX2::MotorsX2(Motor *motorR, Motor *motorL, Timer *timer) 
-                    : _motorR(motorR), _motorL(motorL), _timer(timer) {}
+MotorsX2::MotorsX2(Motor *motorR, Motor *motorL) 
+                    : _motorR(motorR), _motorL(motorL) {}
 
 void MotorsX2::init(uint32_t freq, uint8_t res) {
     _motorR->init(freq, res);
@@ -18,20 +18,4 @@ void MotorsX2::move(int16_t velocity, int16_t turn) {
     int16_t pwmL = constrain(velocity - turn, -MOTOR_MAX_PWM, MOTOR_MAX_PWM);
     _motorR->rotate(pwmR); 
     _motorL->rotate(pwmL); 
-}
-
-bool MotorsX2::moveForMs(int16_t velocity, int16_t turn, uint32_t ms) {
-    if(!_timer)
-        return false;
-    if(_timer->tick()) {
-        stop();
-        _timer->reset();
-        return true;
-    }
-    if(!_timer->isRunning()) {
-        _timer->setTimeout(ms);
-        _timer->start();
-    }
-    move(velocity, turn);
-    return false;
 }
--- lib/Motor/src/MotorsX2.h
+++ lib/Motor/src/MotorsX2.h
@@ -1,18 +1,15 @@
 #ifndef MOTORS_X2_H
 #define MOTORS_X2_H
-#include <Timer.h>
 #include "Motor.h"
 
 class MotorsX2 {
 private:
   Motor *_motorR;
   Motor *_motorL;
-  Timer *_timer;
 public:
-  MotorsX2(Motor *motorR, Motor *motorL, Timer *timer = nullptr);
+  MotorsX2(Motor *motorR, Motor *motorL);
   void init(uint32_t freq, uint8_t res);
   void stop();
   void move(int16_t velocity, int16_t turn);
-  bool moveForMs(int16_t velocity, int16_t turn, uint32_t ms);
 };
 #endif
