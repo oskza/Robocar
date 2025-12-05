7b1f92f77cfde5a2f5a9ccee73142cb4a586241e
oskza <dev@oskza.com>
2025-12-05 22:17:22 +0200

    refactor: added read pwm to AnalogJoystic

--- lib/AnalogJoystic/src/AnalogJoystic.h
+++ lib/AnalogJoystic/src/AnalogJoystic.h
@@ -4,18 +4,20 @@
 
 class AnalogJoystic {
 private:
-  uint8_t _vertPin, _horzPin;
-  uint16_t _deadzone, _vertCenter, _horzCenter;
+  uint8_t _vertPin;
+  uint8_t _horzPin;
+  uint16_t _vertCenter;
+  uint16_t _horzCenter;
+  uint16_t _deadzone;
 public:
   AnalogJoystic(uint8_t vertPin, uint8_t horzPin, uint16_t deadzone = 80);
-
   void calibrateVerticalCenter();
   void calibrateHorizontalCenter();
   void calibrateCenter();
-
-  uint16_t readVertical() const;
-  uint16_t readHorizontal() const;
-
-  bool isInDeadzone(uint16_t vert, uint16_t horz) const;
+  uint16_t readVerticalRaw() const;
+  int16_t readVerticalPWM() const;
+  uint16_t readHorizontalRaw() const;
+  int16_t readHorizontalPWM() const;
+  bool isInDeadzone(int16_t vert, int16_t horz) const;
 };
 #endif
