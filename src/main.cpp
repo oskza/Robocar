1b04eb9452ee2aa9cd796f3d349f2ad3ede93759
oskza <dev@oskza.com>
2025-12-21 14:49:14 +0200

    created wifi manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "WiFi.h"
 #include <MotorsX2.h>
 #include <Encoder.h>
 #include <CompassBMM150.h>
@@ -32,8 +33,57 @@
 #define JOYSTIC_VERT_PIN    34
 #define JOYSTIC_DEADZONE    7
 
+#define LOCAL_IP_1          192
+#define LOCAL_IP_2          168
+#define LOCAL_IP_3          1
+#define LOCAL_IP_4          200
+
+#define GATEWAY_1           192
+#define GATEWAY_2           168
+#define GATEWAY_3           1
+#define GATEWAY_4           1
+
+#define SUBNET_1            255
+#define SUBNET_2            255
+#define SUBNET_3            0
+#define SUBNET_4            0
+
+#define PRIMARY_DNS_1       8
+#define PRIMARY_DNS_2       8
+#define PRIMARY_DNS_3       8
+#define PRIMARY_DNS_4       8
+
+#define SECONDARY_DNS_1     8
+#define SECONDARY_DNS_2     8
+#define SECONDARY_DNS_3     4
+#define SECONDARY_DNS_4     4
+
+#define WIFI_SSID           "dom-24"
+#define WIFI_PASSWORD       "11@ZaHero#99"
+
+#define WIFI_CHECK_MS       3000
+
 #define METERS_TO_TICKS(meters) ((uint32_t)round((double)(meters) / WHEEL_CIRCUMFERENCE * ENCODER_SLOTS))
 #define TICKS_TO_METERS(ticks) (((double)(ticks) / ENCODER_SLOTS) * WHEEL_CIRCUMFERENCE)
+
+struct WifiManager {
+    bool init() {
+        IPAddress localIP(LOCAL_IP_1, LOCAL_IP_2, LOCAL_IP_3, LOCAL_IP_4);
+        IPAddress gateway(GATEWAY_1, GATEWAY_2, GATEWAY_3, GATEWAY_4);
+        IPAddress subnet(SUBNET_1, SUBNET_2, SUBNET_3, SUBNET_4);
+        IPAddress primaryDNS(PRIMARY_DNS_1, PRIMARY_DNS_2, PRIMARY_DNS_3, PRIMARY_DNS_4);
+        IPAddress secondaryDNS(SECONDARY_DNS_1, SECONDARY_DNS_2, SECONDARY_DNS_3, SECONDARY_DNS_4);
+        WiFi.mode(WIFI_STA);
+        return WiFi.config(localIP, gateway, subnet, primaryDNS, secondaryDNS);
+    }
+    wl_status_t connect() {
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        return WiFi.status();
+    }
+    bool checkConnectivity() {
+        return WiFi.status() == WL_CONNECTED || WiFi.reconnect();
+    }
+};
 
 Motor motorR(MOTOR_R_PWM_PIN, MOTOR_R_FWD_PIN, MOTOR_R_BCK_PIN, MOTOR_R_PWM_CHANNEL);
 Motor motorL(MOTOR_L_PWM_PIN, MOTOR_L_FWD_PIN, MOTOR_L_BCK_PIN, MOTOR_L_PWM_CHANNEL, MOTOR_L_CORR);
@@ -46,7 +96,10 @@ CompassBMM150 compass;
 
 AnalogJoystic joystic(JOYSTIC_VERT_PIN, JOYSTIC_HORZ_PIN, JOYSTIC_DEADZONE);
 
-Timer timer;
+Timer timerMotors;
+Timer timerWifi(WIFI_CHECK_MS);
+
+WifiManager wifiManager;
 
 void IRAM_ATTR onRightEncoder() { encoderR.tick(); }
 void IRAM_ATTR onLeftEncoder()  { encoderL.tick(); }
@@ -62,14 +115,14 @@ void joysticHandle() {
 }
 
 bool moveForMs(int16_t velocity, int16_t turn, uint32_t ms) {
-    if(timer.tick()) {
+    if(timerMotors.tick()) {
         motors.stop();
-        timer.reset();
+        timerMotors.reset();
         return true;
     }
-    if(!timer.isRunning()) {
-        timer.setTimeout(ms);
-        timer.start();
+    if(!timerMotors.isRunning()) {
+        timerMotors.setTimeout(ms);
+        timerMotors.start();
     }
     motors.move(velocity, turn);
     return false;
@@ -91,16 +144,24 @@ bool moveForMeters(int16_t velocity, int16_t turn, float meters) {
 void setup() {
     Serial.begin(115200);
 
-    motors.init(MOTORS_PWM_FREQ, MOTORS_PWM_RES);
+    if (!wifiManager.init() || wifiManager.connect() != WL_CONNECTED) {/*...*/}
+    timerWifi.start();
+
+    // motors.init(MOTORS_PWM_FREQ, MOTORS_PWM_RES);
 
     // encoderR.init(onRightEncoder);
     // encoderL.init(onLeftEncoder);
 
     // if (!compass.init()) {/*...*/}
 
-    joystic.calibrateCenter();
+    // joystic.calibrateCenter();
 }
 
 void loop() {
-    joysticHandle();
+    // joysticHandle();
+
+    if (timerWifi.tick()) {
+        wifiManager.checkConnectivity();
+        timerWifi.refresh();
+    }
 }
