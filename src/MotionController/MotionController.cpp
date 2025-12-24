6d6fe8928cf88e2164e201141d6d4aa827474052
oskza <dev@oskza.com>
2025-12-24 23:30:18 +0200

    refactor: added mode and manual moving via websocket

--- src/MotionController/MotionController.cpp
+++ src/MotionController/MotionController.cpp
@@ -2,14 +2,48 @@
 
 MotionController::MotionController(Motor *motorR, Motor *motorL, 
                                     Encoder *encoderR, Encoder *encoderL, Timer *timer) 
-                                : _motorR(motorR), _motorL(motorL), 
-                                    _encoderR(encoderR), _encoderL(encoderL), _timer(timer) {}
+                                : _motorR(motorR), _motorL(motorL), _encoderR(encoderR), _encoderL(encoderL), 
+                                    _timer(timer), _mode(MOTION_MODE_AUTO), _isMoving(false), _ticksToMove(0) {}
+
+void MotionController::_move(int16_t velocity, int16_t turn) {
+    int16_t pwmR = constrain(velocity + turn, -MOTOR_MAX_PWM, MOTOR_MAX_PWM);
+    int16_t pwmL = constrain(velocity - turn, -MOTOR_MAX_PWM, MOTOR_MAX_PWM);
+    _motorR->rotate(pwmR); 
+    _motorL->rotate(pwmL);
+    _isMoving = true;
+}
+
+bool MotionController::validateMode(uint8_t mode) {
+    return mode == MOTION_MODE_AUTO || mode == MOTION_MODE_MANUAL;
+}
 
 void MotionController::init(void (*onRightEncoder)(), void (*onLeftEncoder)(), uint32_t freq, uint8_t res) {
     _motorR->init(freq, res);
     _motorL->init(freq, res);
     _encoderR->init(onRightEncoder);
     _encoderL->init(onLeftEncoder);
+    _timer->reset();
+    _mode = MOTION_MODE_AUTO;
+    _isMoving = false;
+    _ticksToMove = 0;
+}
+
+void MotionController::tick() {
+    if (!_isMoving || _mode == MOTION_MODE_MANUAL)
+        return;
+
+    if (_timer->isRunning() && _timer->tick()) {
+        stop();
+        _timer->reset();
+        return;
+    }
+    
+    if (_ticksToMove > 0 
+            && (_encoderR->getCount() >= _ticksToMove || _encoderL->getCount() >= _ticksToMove)) {
+        stop();
+        _ticksToMove = 0;
+        return;
+    }
 }
 
 void MotionController::stop() {
@@ -17,36 +51,64 @@ void MotionController::stop() {
     _motorL->stop();
     _encoderR->reset();
     _encoderL->reset();
-    _timer->reset();
+    _isMoving = false;
 }
 
-void MotionController::move(int16_t velocity, int16_t turn) {
-    int16_t pwmR = constrain(velocity + turn, -MOTOR_MAX_PWM, MOTOR_MAX_PWM);
-    int16_t pwmL = constrain(velocity - turn, -MOTOR_MAX_PWM, MOTOR_MAX_PWM);
-    _motorR->rotate(pwmR); 
-    _motorL->rotate(pwmL);
+void MotionController::moveManual(int16_t velocity, int16_t turn) {
+    if (_mode == MOTION_MODE_AUTO)
+        return;
+    _move(velocity, turn);
 }
 
-bool MotionController::moveForMs(int16_t velocity, int16_t turn, uint32_t ms) {
-    if(_timer->tick()) {
-        stop();
-        return true;
-    }
-    if(!_timer->isRunning()) {
-        _timer->setTimeout(ms);
-        _timer->start();
-    }
-    move(velocity, turn);
-    return false;
+void MotionController::moveForMs(int16_t velocity, int16_t turn, uint32_t ms) {
+    if (_mode != MOTION_MODE_AUTO || _isMoving)
+        return;
+    _timer->setTimeout(ms);
+    _timer->start();
+    _move(velocity, turn);
 }
 
-bool MotionController::moveForMeters(int16_t velocity, int16_t turn, float meters) {
-    uint32_t ticks = METERS_TO_TICKS(meters);
-    if(_encoderR->getCount() >= ticks 
-            || _encoderL->getCount() >= ticks) {
-        stop();
-        return true;
-    }
-    move(velocity, turn);
-    return false;
+void MotionController::moveForMeters(int16_t velocity, int16_t turn, float meters) {
+    if (_mode != MOTION_MODE_AUTO || _isMoving)
+        return;
+    _ticksToMove = METERS_TO_TICKS(meters);
+    _move(velocity, turn);
+}
+
+void MotionController::moveForward(uint8_t velocity) {
+    if (_mode != MOTION_MODE_MANUAL)
+        return;
+    _move(velocity, 0);
+
+}
+
+void MotionController::moveBackward(uint8_t velocity) {
+    if (_mode != MOTION_MODE_MANUAL)
+        return;
+    _move(-velocity, 0);
+
+}
+
+void MotionController::moveLeft(uint8_t velocity) {
+    if (_mode != MOTION_MODE_MANUAL)
+        return;
+    _move(0, velocity);
+
+}
+
+void MotionController::moveRight(uint8_t velocity) {
+    if (_mode != MOTION_MODE_MANUAL)
+        return;
+    _move(0, -velocity);
+}
+
+bool MotionController::isMoving() const { return _isMoving; }
+
+bool MotionController::isModeAuto() const { return _mode == MOTION_MODE_AUTO; }
+
+bool MotionController::isModeManual() const { return _mode == MOTION_MODE_MANUAL; }
+
+void MotionController::setMode(uint8_t mode) {
+    if (validateMode(mode))
+        _mode = mode;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,37 +8,37 @@
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
 
-#define MOTOR_R_PWM_PIN     25
-#define MOTOR_R_FWD_PIN     18
-#define MOTOR_R_BCK_PIN     19
+#define MOTOR_R_PWM_PIN         25
+#define MOTOR_R_FWD_PIN         18
+#define MOTOR_R_BCK_PIN         19
 
-#define MOTOR_L_PWM_PIN     26
-#define MOTOR_L_FWD_PIN     27
-#define MOTOR_L_BCK_PIN     14
+#define MOTOR_L_PWM_PIN         26
+#define MOTOR_L_FWD_PIN         27
+#define MOTOR_L_BCK_PIN         14
 
-#define MOTOR_R_PWM_CHANNEL 0
-#define MOTOR_L_PWM_CHANNEL 1
+#define MOTOR_R_PWM_CHANNEL     0
+#define MOTOR_L_PWM_CHANNEL     1
 
-#define MOTOR_PWM_FREQ     1000
-#define MOTOR_PWM_RES      8
+#define MOTOR_PWM_FREQ          1000
+#define MOTOR_PWM_RES           8
 
-#define MOTOR_L_CORR        1.0395f
+#define MOTOR_L_CORR            1.0395f
 
-#define ENCODER_R_PIN       32
-#define ENCODER_L_PIN       33
+#define ENCODER_R_PIN           32
+#define ENCODER_L_PIN           33
 
-#define JOYSTIC_VERT_PIN    34
-#define JOYSTIC_HORZ_PIN    35
-#define JOYSTIC_DEADZONE    7
-#define JOYSTIC_INTERVAL_MS 20
+#define JOYSTIC_VERT_PIN        34
+#define JOYSTIC_HORZ_PIN        35
+#define JOYSTIC_DEADZONE        7
+#define JOYSTIC_INTERVAL_MS     20
 
-#define WIFI_INTERVAL_MS 3000
+#define WIFI_INTERVAL_MS        3000
 
-#define WEBSOCKET_PORT        80
-#define WEBSOCKET_MAX_CLIENTS 2
-#define WEBSOCKET_INTERVAL_MS 2000
+#define WEBSOCKET_PORT          80
+#define WEBSOCKET_MAX_CLIENTS   2
+#define WEBSOCKET_INTERVAL_MS   2000
 
-#define REPORT_INTERVAL_MS    5000
+#define REPORT_INTERVAL_MS      5000
 
 Timer timerMotion, timerJoystic, timerWifi, timerWebSocket, timerReport;
 
@@ -65,20 +65,45 @@ void handleCommand(JsonDocument &doc) {
     const char* cmd = doc["cmd"];
     if (!cmd) 
         return;
-
+    if (strcmp(cmd, "set-mode") == 0) {
+        motionController.setMode(doc["payload"]);
+        return;
+    }
     if (strcmp(cmd, "stop") == 0) {
         motionController.stop();
         return;
     }
     if (strcmp(cmd, "move") == 0) {
-        int velocity = doc["payload"]["velocity"] | 0;
-        int turn = doc["payload"]["turn"] | 0;
+
+        if(motionController.isModeManual()) {
+            const char *dir = doc["payload"];
+
+            if (strcmp(dir, "forward") == 0) {
+                motionController.moveForward();
+                return;
+            }
+            if (strcmp(dir, "backward") == 0) {
+                motionController.moveBackward();
+                return;
+            }
+            if (strcmp(dir, "left") == 0) {
+                motionController.moveLeft();
+                return;
+            }
+            if (strcmp(dir, "right") == 0) {
+                motionController.moveRight();
+            }
+            return;
+        }
+
+        int16_t velocity = doc["payload"]["velocity"] | 0;
+        int16_t turn = doc["payload"]["turn"] | 0;
         JsonObject dur = doc["payload"]["duration"];
-        const char* durUnit = dur["unit"] | "ms";
+        const char *durUnit = dur["unit"] | "ms";
         float durValue = dur["value"] | 0;
 
         if (durValue == 0) {
-            motionController.move(velocity, turn);
+            motionController.moveManual(velocity, turn);
             return;
         }
 
@@ -169,13 +194,15 @@ void createReport(JsonDocument &doc) {
         wifi["ip"] = wifiController.getIP();
         wifi["rssi"] = wifiController.getRSSI();
 
+    doc["clients"] = ws.count();
+
     JsonObject memory = doc.createNestedObject("memory");
     memory["free"] = ESP.getFreeHeap();
     memory["total"] = ESP.getHeapSize();
     memory["maxAlloc"] = ESP.getMaxAllocHeap();
 
+    doc["mode"] = (motionController.isModeAuto()) ? "auto" : "manual";
     doc["heading"] = compass.getCompassDegree();
-    doc["clients"] = ws.count();
 }
 
 void sendReport() {
@@ -208,15 +235,18 @@ void setup() {
 }
 
 void loop() {
-    int16_t vert, horz;
-    if(joysticController.tick(vert, horz)) {
-        if(joysticController.isInDeadzone(vert, horz)) {
-            motionController.stop();
-        } else {
-            motionController.move(vert, horz);
-        }
+    if (motionController.isModeManual()) {
+        // int16_t vert, horz;
+        // if(joysticController.tick(vert, horz)) {
+        //     if(joysticController.isInDeadzone(vert, horz))
+        //         motionController.stop();
+        //     else
+        //         motionController.moveManual(vert, horz);
+        // }
+    } else {
+        motionController.tick();
     }
-
+    
     if (wifiController.tick()) {/*...*/}
 
     if (timerWebSocket.tick()) {
