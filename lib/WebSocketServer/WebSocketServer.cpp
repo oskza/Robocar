3f981648c3bc6090949a21b1469efdeeb71a9b4b
oskza <oskzadev@gmail.com>
2026-07-02 18:37:49 +0300

    refactored WebSocketServer. added message callback

--- lib/WebSocketServer/WebSocketServer.cpp
+++ lib/WebSocketServer/WebSocketServer.cpp
@@ -1,19 +1,78 @@
 #include "WebSocketServer.h"
 
-WebSocketServer::WebSocketServer(uint16_t port, const char *path) : _ws(path), _server(port) {}
+WebSocketServer::WebSocketServer(uint16_t port, const char *path, uint8_t maxClients)
+    : _server(port),
+      _ws(path),
+      _maxClients(maxClients),
+      _messageCallback(nullptr) {}
 
 void WebSocketServer::begin() {
+    _ws.onEvent([this](
+            AsyncWebSocket*, AsyncWebSocketClient *client,
+            AwsEventType type, void *arg, uint8_t *data, size_t len) {
+        _handleEvent(client, type, arg, data, len);
+    });
     _server.addHandler(&_ws);
     _server.begin();
 }
 
-void WebSocketServer::update() { _ws.cleanupClients(); }
+void WebSocketServer::update() { _ws.cleanupClients(_maxClients); }
 
-bool WebSocketServer::hasClients() const { return _ws.count() > 0; }
+bool WebSocketServer::hasClients() const { return getClientCount() > 0; }
+
+uint8_t WebSocketServer::getClientCount() const { return (uint8_t)_ws.count(); }
+
+bool WebSocketServer::send(AsyncWebSocketClient *client, const char *message, size_t length) {
+    if (!client || !message || length == 0)
+        return false;
+    client->text(message, length);
+    return true;
+}
 
 bool WebSocketServer::broadcast(const char *message) {
-    if (!message || !hasClients())
+    if (!message)
         return false;
-    _ws.textAll(message);
+    return broadcast(message, strlen(message));
+}
+
+bool WebSocketServer::broadcast(const char *message, size_t length) {
+    if (!message || length == 0 || !hasClients())
+        return false;
+    _ws.textAll(message, length);
     return true;
+}
+
+void WebSocketServer::setMessageCallback(MessageCallback callback) { _messageCallback = callback; }
+
+void WebSocketServer::_handleEvent(AsyncWebSocketClient *client, AwsEventType type,
+                                    void *arg, uint8_t *data, size_t len) {
+    switch (type) {
+        case WS_EVT_CONNECT:
+            _handleConnect(client);
+            break;
+        case WS_EVT_DATA:
+            _handleData(arg, data, len);
+            break;
+        default:
+            break;
+    }
+}
+
+void WebSocketServer::_handleConnect(AsyncWebSocketClient *client) {
+    if (!client)
+        return;
+    client->setCloseClientOnQueueFull(false);
+    client->ping();
+}
+
+void WebSocketServer::_handleData(void *arg, uint8_t *data, size_t len) {
+    if (!_messageCallback)
+        return;
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
+    if (!info || !info->final
+            || info->index != 0
+            || info->len != len
+            || info->opcode != WS_TEXT)
+        return;
+    _messageCallback((const char *)data, len);
 }
--- lib/WebSocketServer/WebSocketServer.h
+++ lib/WebSocketServer/WebSocketServer.h
@@ -1,17 +1,28 @@
 #ifndef WEB_SOCKET_SERVER_H
 #define WEB_SOCKET_SERVER_H
-#include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 
 class WebSocketServer {
-private:
-    AsyncWebSocket _ws;
-    AsyncWebServer _server;
 public:
-    explicit WebSocketServer(uint16_t port = 80, const char *path = "/ws");
+    using MessageCallback = void (*)(const char *message, size_t length);
+    explicit WebSocketServer(uint16_t port = 80, const char *path = "/ws",
+                                uint8_t maxClients = 4);
     void begin();
     void update();
     bool hasClients() const;
+    uint8_t getClientCount() const;
+    bool send(AsyncWebSocketClient *client, const char *message, size_t length);
     bool broadcast(const char *message);
+    bool broadcast(const char *message, size_t length);
+    void setMessageCallback(MessageCallback callback);
+private:
+    AsyncWebServer _server;
+    AsyncWebSocket _ws;
+    uint8_t _maxClients;
+    MessageCallback _messageCallback;
+    void _handleEvent(AsyncWebSocketClient *client, AwsEventType type,
+                        void *arg, uint8_t *data, size_t len);
+    void _handleConnect(AsyncWebSocketClient *client);
+    void _handleData(void *arg, uint8_t *data, size_t len);
 };
 #endif
