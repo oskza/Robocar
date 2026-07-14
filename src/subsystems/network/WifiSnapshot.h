50413c5516ff183bebed2f853f8e016f4309fc58
oskza <oskzadev@gmail.com>
2026-07-14 18:58:06 +0300

    named WifiStationSnapshot and WifiAccessPointSnapshot structs in WifiSnapshot

--- src/subsystems/network/WifiSnapshot.h
+++ src/subsystems/network/WifiSnapshot.h
@@ -10,14 +10,14 @@
 struct WifiSnapshot {
     WifiMode mode;
     char hostname[WifiConfig::HOSTNAME_MAX_LEN + 1];
-    struct {
+    struct WifiStationSnapshot {
         WifiStationState state;
         IPAddress ip;
         char ssid[WifiCredentials::SSID_MAX_LEN + 1];
         int8_t rssi;
         uint32_t reconnectAttempts;
     } station;
-    struct {
+    struct WifiAccessPointSnapshot {
         WifiAccessPointState state;
         IPAddress ip;
         char ssid[WifiCredentials::SSID_MAX_LEN + 1];
