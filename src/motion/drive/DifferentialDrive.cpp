dc4b089bfb585390b0f608357d831170b70593b8
oskza <oskzadev@gmail.com>
2026-07-17 00:27:06 +0300

    created _normalizeOutputs private method @ DifferentialDrive

--- src/motion/drive/DifferentialDrive.cpp
+++ src/motion/drive/DifferentialDrive.cpp
@@ -1,7 +1,15 @@
 #include "DifferentialDrive.h"
 
-DifferentialDrive::DifferentialDrive(WheelOutputController &leftWheel, WheelOutputController &rightWheel)
+DifferentialDrive::DifferentialDrive(WheelActuator &leftWheel, WheelActuator &rightWheel)
     : _leftWheel(leftWheel), _rightWheel(rightWheel) {}
+
+void DifferentialDrive::_normalizeOutputs(int16_t &leftOutput, int16_t &rightOutput) {
+    const int16_t maxMagnitude = max(abs(leftOutput), abs(rightOutput));
+    if (maxMagnitude <= MAX_OUTPUT)
+        return;
+    leftOutput = (int32_t(leftOutput) * MAX_OUTPUT) / maxMagnitude;
+    rightOutput = (int32_t(rightOutput) * MAX_OUTPUT) / maxMagnitude;
+}
 
 void DifferentialDrive::begin(
     uint8_t acceleration,
@@ -13,29 +21,19 @@ void DifferentialDrive::begin(
     _rightWheel.begin(acceleration, pwmFrequency, rightMinEffectivePwm);
 }
 
-bool DifferentialDrive::isStopped() const { return _leftWheel.isStopped() && _rightWheel.isStopped(); }
-
-int16_t DifferentialDrive::getLeftOutput() const { return _leftWheel.getCurrentOutput(); }
-
-int16_t DifferentialDrive::getRightOutput() const { return _rightWheel.getCurrentOutput(); }
-
-void DifferentialDrive::drive(int16_t velocity, int16_t turn) {
-    velocity = constrain(velocity, -MotorDriver::MAX_OUTPUT, MotorDriver::MAX_OUTPUT);
-    turn = constrain(turn, -MotorDriver::MAX_OUTPUT, MotorDriver::MAX_OUTPUT);
-    int16_t rightOutput = velocity + turn;
-    int16_t leftOutput  = velocity - turn;
-    int16_t maxMagnitude = max(abs(leftOutput), abs(rightOutput));
-    if (maxMagnitude > MotorDriver::MAX_OUTPUT) {
-        leftOutput  = (leftOutput  * MotorDriver::MAX_OUTPUT) / maxMagnitude;
-        rightOutput = (rightOutput * MotorDriver::MAX_OUTPUT) / maxMagnitude;
-    }
-    _leftWheel.setTargetOutput(leftOutput);
-    _rightWheel.setTargetOutput(rightOutput);
-}
-
 void DifferentialDrive::update() {
     _leftWheel.update();
     _rightWheel.update();
+}
+
+void DifferentialDrive::drive(int16_t velocity, int16_t turn) {
+    velocity = constrain(velocity, -MAX_OUTPUT, MAX_OUTPUT);
+    turn = constrain(turn, -MAX_OUTPUT, MAX_OUTPUT);
+    int16_t leftOutput = velocity - turn;
+    int16_t rightOutput = velocity + turn;
+    _normalizeOutputs(leftOutput, rightOutput);
+    _leftWheel.setTargetOutput(leftOutput);
+    _rightWheel.setTargetOutput(rightOutput);
 }
 
 void DifferentialDrive::stop() {
@@ -47,3 +45,9 @@ void DifferentialDrive::brake() {
     _leftWheel.brake();
     _rightWheel.brake();
 }
+
+bool DifferentialDrive::isStopped() const { return _leftWheel.isStopped() && _rightWheel.isStopped(); }
+
+int16_t DifferentialDrive::getLeftOutput() const { return _leftWheel.getCurrentOutput(); }
+
+int16_t DifferentialDrive::getRightOutput() const { return _rightWheel.getCurrentOutput(); }
--- src/motion/drive/DifferentialDrive.h
+++ src/motion/drive/DifferentialDrive.h
@@ -1,25 +1,27 @@
 #ifndef DIFFERENTIAL_DRIVE_H
 #define DIFFERENTIAL_DRIVE_H
-#include "../wheel/WheelOutputController.h"
+#include "../wheel/WheelActuator.h"
 
 class DifferentialDrive {
 private:
-    WheelOutputController &_leftWheel;
-    WheelOutputController &_rightWheel;
+    static constexpr int16_t MAX_OUTPUT = 255;
+    WheelActuator &_leftWheel;
+    WheelActuator &_rightWheel;
+    static void _normalizeOutputs(int16_t &leftOutput, int16_t &rightOutput);
 public:
-    DifferentialDrive(WheelOutputController &leftWheel, WheelOutputController &rightWheel);
+    DifferentialDrive(WheelActuator &leftWheel, WheelActuator &rightWheel);
     void begin(
         uint8_t acceleration,
         uint32_t pwmFrequency,
         uint8_t leftMinEffectivePwm = 0,
         uint8_t rightMinEffectivePwm = 0
     );
+    void update();
+    void drive(int16_t velocity, int16_t turn);
+    void stop();
+    void brake();
     bool isStopped() const;
     int16_t getLeftOutput() const;
     int16_t getRightOutput() const;
-    void drive(int16_t velocity, int16_t turn);
-    void update();
-    void stop();
-    void brake();
 };
 #endif
