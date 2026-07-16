24eec221e05fb3a2654888eb22c2f117bd7fd0df
oskza <oskzadev@gmail.com>
2026-07-17 00:30:22 +0300

    added heading.available propery and init values to MotionSnapshot

--- src/motion/MotionSnapshot.h
+++ src/motion/MotionSnapshot.h
@@ -3,20 +3,21 @@
 #include "MotionState.h"
 
 struct MotionSnapshot {
-    MotionState state;
-    bool stopped;
+    MotionState state = MotionState::IDLE;
+    bool stopped = true;
     struct MotionOutputSnapshot {
-        int16_t left;
-        int16_t right;
+        int16_t left = 0;
+        int16_t right = 0;
     } output;
     struct MotionHeadingSnapshot {
-        float currentDegrees;
-        float targetDegrees;
-        float errorDegrees;
+        bool available = false;
+        float currentDegrees = 0.0f;
+        float targetDegrees = 0.0f;
+        float errorDegrees = 0.0f;
     } heading;
     struct MotionOdometrySnapshot {
-        float distanceMeters;
-        uint32_t averageTicks;
+        float distanceMeters = 0.0f;
+        uint32_t averageTicks = 0;
     } odometry;
 };
 #endif
