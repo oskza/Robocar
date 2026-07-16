3a86bd39cd2d23011758289b1f226e806b7babec
oskza <oskzadev@gmail.com>
2026-07-17 00:31:06 +0300

    edited text formatting MotionSate

--- src/motion/MotionState.h
+++ src/motion/MotionState.h
@@ -2,5 +2,11 @@
 #define MOTION_STATE_H
 #include <stdint.h>
 
-enum class MotionState : uint8_t { IDLE, MANUAL, TIMED, DISTANCE, ROTATING };
+enum class MotionState : uint8_t {
+    IDLE,
+    MANUAL,
+    TIMED,
+    DISTANCE,
+    ROTATING
+};
 #endif
