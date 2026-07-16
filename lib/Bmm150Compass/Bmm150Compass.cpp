8c781722b73118ac3ed20750e51dd269197df86a
oskza <oskzadev@gmail.com>
2026-07-17 00:17:51 +0300

    refactor: replaced getHeadingDegreess with readHeadingDegrees and edited constructor

--- lib/Bmm150Compass/Bmm150Compass.cpp
+++ lib/Bmm150Compass/Bmm150Compass.cpp
@@ -1,8 +1,7 @@
 #include "Bmm150Compass.h"
 
-Bmm150Compass::Bmm150Compass()
-    : _sensor(&Wire, I2C_ADDRESS_4),
-        _available(false) {}
+Bmm150Compass::Bmm150Compass(TwoWire &wire, uint8_t address)
+    : _sensor(&wire, address), _available(false) {}
 
 bool Bmm150Compass::begin() {
     if (_sensor.begin() != 0) {
@@ -19,4 +18,9 @@ bool Bmm150Compass::begin() {
 
 bool Bmm150Compass::isAvailable() const { return _available; }
 
-float Bmm150Compass::getHeadingDegrees() { return _sensor.getCompassDegree(); }
+bool Bmm150Compass::readHeadingDegrees(float &headingDegrees) {
+    if (!_available)
+        return false;
+    headingDegrees = _sensor.getCompassDegree();
+    return true;
+}
--- lib/Bmm150Compass/Bmm150Compass.h
+++ lib/Bmm150Compass/Bmm150Compass.h
@@ -1,15 +1,16 @@
 #ifndef BMM150_COMPASS_H
 #define BMM150_COMPASS_H
 #include <DFRobot_BMM150.h>
+#include <Wire.h>
 
 class Bmm150Compass {
 private:
     DFRobot_BMM150_I2C _sensor;
     bool _available;
 public:
-    Bmm150Compass();
+    explicit Bmm150Compass(TwoWire &wire = Wire, uint8_t address = I2C_ADDRESS_4);
     bool begin();
     bool isAvailable() const;
-    float getHeadingDegrees();
+    bool readHeadingDegrees(float &headingDegrees);
 };
 #endif
